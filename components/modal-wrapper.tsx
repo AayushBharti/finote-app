517b8aabfc92c4f3ea05b4746dda72e6f3cc1849
AayushBharti <iaayushbharti@gmail.com>
2025-11-15 02:00:01 +0530

    Refactor ModalWrapper component in modal-wrapper.tsx: streamline import statements, improve code readability, and adjust padding for iOS devices.

--- components/modal-wrapper.tsx
+++ components/modal-wrapper.tsx
@@ -1,9 +1,9 @@
-import { Platform, StyleSheet, View } from "react-native";
-import React from "react";
-import { colors, spacingY } from "../constants/theme";
-import { ModalWrapperProps } from "../types";
+import React from "react"
+import { Platform, StyleSheet, View } from "react-native"
+import { colors, spacingY } from "../constants/theme"
+import { ModalWrapperProps } from "../types"
 
-const isIos = Platform.OS == "ios";
+const isIos = Platform.OS === "ios"
 const ModalWrapper = ({
   style,
   bg = colors.neutral800,
@@ -13,16 +13,16 @@ const ModalWrapper = ({
     <View style={[styles.container, style && style, { backgroundColor: bg }]}>
       {children}
     </View>
-  );
-};
+  )
+}
 
-export default ModalWrapper;
+export default ModalWrapper
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     // marginTop: spacingY._10,
-    paddingTop: isIos ? spacingY._15 : 60,
+    paddingTop: isIos ? spacingY._30 : 60,
     paddingBottom: isIos ? spacingY._20 : spacingY._20,
   },
-});
+})
